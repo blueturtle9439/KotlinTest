21ff51a68bcf1e440e9a9dfbae3c5ba287fa1244
blueturtle9439 <23012115+blueturtle9439@users.noreply.github.com>
2018-07-23 08:02:43 +0900

    Update test001.kt

--- src/test001.kt
+++ src/test001.kt
@@ -1,16 +1,6 @@
 fun main(args: Array<String>) {
     val car = Car()
-    println(car.getBrandName())
+ 
 }
 
-class Car {
-
-    fun getBrandName() : String {
-        return "BMW"
-    }
-
-}
-fun Car.getPrice() : Int {
-    return 10000
-}
 
